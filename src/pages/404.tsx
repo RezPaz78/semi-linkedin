4b02d5f5d928037dda746208e19364db08cc45fa
reza pazan <rezapazan@rocketmail.com>
2021-10-27 09:46:00 +0330

    navbar paging done, notif icon added

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFound: React.FC = () => {
-  return <h2>اینجا چیزی نیست :(</h2>;
+  return <h2 className="notFound">اینجا چیزی نیست :(</h2>;
 };
 
 export default NotFound;
--- src/shared/components/layout/components/Navbar.tsx
+++ src/shared/components/layout/components/Navbar.tsx
@@ -3,6 +3,7 @@ import Logo from "src/assets/img/logo.png";
 import Home from "src/assets/svg/home.svg";
 import Bell from "src/assets/svg/bell.svg";
 import Group from "src/assets/svg/group.svg";
+import { NavLink } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
@@ -11,9 +12,29 @@ const Navbar: React.FC = () => {
         <img src={Logo} alt="logo" />
       </div>
       <div className="navbar__pages">
-        <img className="navbar__pages-home" src={Home} alt="home" />
-        <img className="navbar__pages-bell" src={Bell} alt="bell" />
-        <img className="navbar__pages-group" src={Group} alt="group" />
+        <NavLink
+          to="/"
+          exact
+          className="navbar__pages_container"
+          activeClassName="navbar__pages_container-active"
+        >
+          <img className="navbar__pages-home" src={Home} alt="home" />
+        </NavLink>
+        <NavLink
+          to="/notifications"
+          className="navbar__pages_container"
+          activeClassName="navbar__pages_container-active"
+        >
+          <img className="navbar__pages-bell" src={Bell} alt="bell" />
+          <span className="navbar__pages-notification-count">+9</span>
+        </NavLink>
+        <NavLink
+          to="/groups"
+          className="navbar__pages_container"
+          activeClassName="navbar__pages_container-active"
+        >
+          <img className="navbar__pages-group" src={Group} alt="group" />
+        </NavLink>
       </div>
     </nav>
   );
